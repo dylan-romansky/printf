9362365afac0b22cfe80d86f610939882d3888e9
Dylan Romansky <dromansk@e1z2r5p14.42.us.org>
2019-01-15 18:40:56 -0800

    added a check function to verify proper format flags

--- printf/src/parsing_and_handling/checks.c
+++ printf/src/parsing_and_handling/checks.c
@@ -5,32 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dromansk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/15 15:06:17 by dromansk          #+#    #+#             */
-/*   Updated: 2019/01/15 17:49:56 by dromansk         ###   ########.fr       */
+/*   Created: 2019/01/15 18:32:37 by dromansk          #+#    #+#             */
+/*   Updated: 2019/01/15 18:38:35 by dromansk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
-#include "libft.h"
-
-int		check_arg(char *format)
+int		check_flag(char *format)
 {
-	format += flag_skip(format);
-	if (!*format)
+	while (*format && (*format == '-' || *format == '+' || *format == ' ' ||
+				(*format >= '0' && *format <= '9') || *format == '#' ||
+				*format == 'h' || *format == 'l' || *format == 'L' ||
+				*format == 'z' || *format == 'j'))
+		format++;
+	if (!(*format == 'd' || *format == 'i' || *format == 'u' ||
+				*format == 'o' || *format == 'x' || *format == 'X' ||
+				*format == 'c' || *format == 's' || *format == 'p' ||
+				*format == 'f' || *format == '%' || *format == 'U'))
 		return (0);
-	format--;
-	if (*format == '-' || *format == ' ' || (*format <= 9 &&
-				*format >= '0') || *format == '#' || *format == '+' ||
-				*format == '.' || *format == '*' || *format == 'l' ||
-				*format == 'h' || *format == 'L' || *format == 'j' ||
-				*format == 'z')
-		return (1);
-	else
-		return (0);
-}
-
-int		error_manager(char **buf)
-{
-	*buf = ft_strdup("");
-	return (0);
+	return (1);
 }
