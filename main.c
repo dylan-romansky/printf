61dd2050cf5954d990d29172cdd541fe573bf259
Dylan Romansky <dromansk@e1z3r5p22.42.us.org>
2019-02-05 16:49:35 -0800

    fixed user data /colour issue

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int		main(int ac, char **av)
 {
-	char *filename = "420bignig";
+	char *filename = "420";
 	char *str = "chumbles";
-	ft_printf("Le fichier{cyan}%s{eoc} contient : {red}%s{eoc}", filename, str);
+	ft_printf("Le fichier{cyan}%s{eoc} contient : {red}%s{eoc}\n", filename, str);
 	return (0);
 }
